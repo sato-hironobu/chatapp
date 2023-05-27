f3cbdef77c030a0a02eabebd9b1f4c4d2a7cd967
Hironobu <hironobu.sato.r8@gmail.com>
2023-05-27 14:07:40 +0900

    New Commit

--- test/settings.py
+++ test/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
